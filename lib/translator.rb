24c260b74e57fea63cf7353ca32b1ae3836ee1ad
Merjen Charyyeva <merjen.chary@gmail.com>
2019-03-10 22:50:43 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -30,5 +30,4 @@ def get_english_meaning(emoticon_file, emoticon)
   emoticon_lib = load_library(emoticon_file)
   english_meaning = emoticon_lib['get_meaning'][emoticon]
   english_meaning ? english_meaning : 'Sorry, that emoticon was not found'
-end	
 end
